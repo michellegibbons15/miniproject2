04307b28e95d2e9e5b0d83ceb621669d3f9d21a3
MichelleGibbons15 <mcgibbons615@gmail>
2025-02-18 17:17:16 -0500

    Framework

---
+++ Mini-Project-2/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Dashboard from './Pages/Dashboard'
+import Explore from './Pages/Explore'
+import Landing from './Pages/Landing'
+import Login from './Pages/Login'
+import 'leaflet/dist/leaflet.css';
+
+
+function App() {
+
+  return (
+    <>
+    {/* <Landing/> */}
+    {/* <Login/> */}
+    <Dashboard/>
+    {/* <Explore/> */}
+    </>
+  )
+}
+
+export default App
